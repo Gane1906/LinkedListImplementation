456aaebe6a352fe50dc19fdaadd8de5f5bad5f7c
Ganesh Doppani <ganesh.doppani@gmail.com>
2023-03-22 12:47:52 +0530

    [Ganesh] Add:Linked list file is added

---
+++ LinlkedListImplementation/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace LinkedListImnplementation
+{
+    public class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Welcome to Linked List implementation!");
+        }
+    }
+}
